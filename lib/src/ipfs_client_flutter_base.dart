6d3fdbeed40113a2e53f5529b0b7078b2cfeae88
dvird <42498461+dvird@users.noreply.github.com>
2022-03-18 10:48:39 -0500

    adding the most important function: "add"

--- lib/src/ipfs_client_flutter_base.dart
+++ lib/src/ipfs_client_flutter_base.dart
@@ -11,6 +11,29 @@ class IpfsClient {
 
   IpfsClient({this.url = 'http://127.0.0.1:5001', this.authorizationToken});
 
+  Future<dynamic> add(
+      {required Map data}) async {
+    _checkDir(dir);
+    var params = {
+      'arg': dir,
+      'create': true,
+    };
+    try {
+      var response = await _ipfsService.postFile(
+          url: '$url/api/v0/add?',
+          formData: await FormData.fromMap(data),
+          queryParameters: params,
+          authorizationToken: authorizationToken);
+      return response;
+    } on DioError catch (e) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx and is also not 304.
+      if (e.response != null) {
+        return e.response?.data;
+      }
+    }
+  }
+  
   /// Make a directory in IPFS
   /// For more: https://docs.ipfs.io/reference/http/api/#api-v0-files-mkdir
   Future<dynamic> mkdir({required String dir}) async {
